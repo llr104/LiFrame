1f6994904ae893e3edaa27e57e321903b5802f45
llr104 <947491240@qq.com>
2020-02-20 19:40:36 +0800

    中途误删了脚本，补回来

---
+++ kill.bat
@@ -0,0 +1,7 @@
+taskkill /f /t /im loginserver.exe > nul 2> nul 
+taskkill /f /t /im gateserver.exe > nul 2> nul 
+taskkill /f /t /im masterserver.exe > nul 2> nul 
+taskkill /f /t /im worldserver.exe > nul 2> nul 
+taskkill /f /t /im gameserver.exe > nul 2> nul 
+
+pause
---
+++ run.bat
@@ -0,0 +1,5 @@
+start loginserver.exe conf/login.json
+start gateserver.exe conf/gate.json
+start masterserver.exe conf/master.json
+start worldserver.exe conf/world.json
+start gameserver.exe conf/game.json
